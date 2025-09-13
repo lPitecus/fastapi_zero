d0a594edf2b53726e630698e0387f4a43c8c8d6f
lPitecus <tuzaum.silva@gmail.com>
2025-09-13 20:41:41 -0300

    Ajustes no arquivo de teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
@@ -8,3 +10,4 @@ def test_root_deve_retornar_ola_mundo():
     response = client.get('/')
 
     assert response.json() == {'message': 'Olá mundo!'}
+    assert response.status_code == HTTPStatus.OK
