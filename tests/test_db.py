9e76976bea349a692b9c7a6d4ad5b7947d0c5ecb
lPitecus <tuzaum.silva@gmail.com>
2025-09-29 23:30:31 -0300

    adiciona mock de tempo no teste do banco de dados

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,19 +1,28 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fastapi_zero.models import User
 
 
-def test_create_user(session):
+def test_create_user(session, mock_db_time):
     # Define qual usuário será criado no banco de dados,
     # baseado na estrutura do User definida no models.py
-    new_user = User(username='test', email='test@test', password='test')
+    with mock_db_time(model=User) as time:
+        new_user = User(username='test', email='test@test', password='test')
 
-    # Declara que quer adicionar esse usuário no banco de dados
-    session.add(new_user)
+        # Declara que quer adicionar esse usuário no banco de dados
+        session.add(new_user)
 
-    # De fato insere esse usuário no banco de dados
-    session.commit()
+        # De fato insere esse usuário no banco de dados
+        session.commit()
 
-    # Faz uma busca do usuário criado no banco
-    user = session.scalar(select(User).where(User.username == 'test'))
-    assert user.username == 'test'
+        # Faz uma busca do usuário criado no banco
+        user = session.scalar(select(User).where(User.username == 'test'))
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test',
+        'password': 'test',
+        'created_at': time,
+    }
